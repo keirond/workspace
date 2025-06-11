9b72a4aeb414099a06a7063f6d1da008ad58e6df
Bao Dang <dang.hbaokody@gmail.com>
2025-06-11 20:59:04 +0700

    up Wed Jun 11 01:59:04 PM UTC 2025

--- kubernetes/ansible/setup.sh
+++ kubernetes/ansible/setup.sh
@@ -4,12 +4,10 @@ INVENTORY_FILE="inventory/hosts"
 SSH_KEY_PATH="$HOME/.ssh/id_rsa.pub"
 
 while IFS= read -r line || [[ -n "$line" ]]; do
-	# Skip blank lines or section headers like [masters]
 	if [[ "$line" =~ ^\s*$ || "$line" =~ ^\s*\[.*\]\s*$ ]]; then
 		continue
 	fi
 
-	echo "$line"
 	ip=$(echo "$line" | sed -n 's/.*ansible_host=\([0-9.]*\).*/\1/p')
 	user=$(echo "$line" | sed -n 's/.*ansible_user=\([a-zA-Z0-9_-]*\).*/\1/p')
 
