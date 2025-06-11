63b2439c792323f03a1339705f1f48dc9a9b2d4c
Bao Dang <dang.hbaokody@gmail.com>
2025-06-11 20:41:18 +0700

    up Wed Jun 11 01:41:18 PM UTC 2025

--- kubernetes/ansible/setup.sh
+++ kubernetes/ansible/setup.sh
@@ -2,11 +2,24 @@
 
 INVENTORY_FILE="inventory/hosts"
 SSH_KEY_PATH="$HOME/.ssh/id_rsa.pub"
-USER="root"
 
-grep 'ansible_host=' "$INVENTORY_FILE" | while read -r line; do
+exec 3< "$INVENTORY_FILE"
+while read -r line <&3; do
     ip=$(echo "$line" | sed -n 's/.*ansible_host=\([0-9.]*\).*/\1/p')
     user=$(echo "$line" | sed -n 's/.*ansible_user=\([a-zA-Z0-9_-]*\).*/\1/p')
-    echo "Copying SSH key to $user@$ip..."
-    ssh -o PasswordAuthentication=no "$user@$ip" "exit" 2>/dev/null || ssh-copy-id -i "$SSH_KEY_PATH" "$user@$ip"
+
+    if [[ -n "$ip" && -n "$user" ]]; then
+        echo -n "Checking $user@$ip... "
+        if ssh -o PasswordAuthentication=no "$user@$ip" "exit" 2>/dev/null; then
+            echo "already configured."
+        else
+            echo "copying SSH key."
+            if ssh-copy-id -i "$SSH_KEY_PATH" "$user@$ip"; then
+                echo "  [O] Updated $user@$ip"
+            else
+                echo "  [X] Failed to update $user@$ip"
+            fi
+        fi
+    fi
 done
+exec 3<&-
