93dbdb88d4ee738af358848a34bb110efee7017e
Crix Dang <crix.dang@gmail.com>
2025-10-28 12:55:50 +0700

    up Tue Oct 28 05:55:50 AM UTC 2025

--- servers/ansible-for-oracle-hosts/cmd/clean-install.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-ansible-playbook -i inventory/hosts playbook/stop-k8s-docker-podman.yaml
-ansible-playbook -i inventory/hosts playbook/wipe-k8s-docker-podman.yaml
-ansible-playbook -i inventory/hosts playbook/install-k8s-dependencies.yaml
-ansible-playbook -i inventory/hosts playbook/install-k8s-main-master.yaml
-ansible-playbook -i inventory/hosts playbook/install-k8s-join-masters.yaml
-ansible-playbook -i inventory/hosts playbook/install-k8s-join-workers.yaml
