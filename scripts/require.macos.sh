3288e8f3cbd8d19f692f6d47d59d8ed4c122e3a6
Keiron <keiron.dang@gmail.com>
2025-11-22 22:39:15 +0700

    up Sat Nov 22 03:39:15 PM UTC 2025

--- scripts/require.macos.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-
-if ! command -v brew &> /dev/null; then
-    echo "Homebrew not found. Installing..."
-    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-fi
-
-brew install make
