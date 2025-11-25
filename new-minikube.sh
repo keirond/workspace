6e96647ce8da12ee5a7e4079342807bca716825f
Keiron <keiron.dang@gmail.com>
2025-11-25 15:42:27 +0700

    up Tue Nov 25 08:42:27 AM UTC 2025

--- new-minikube.sh
+++ new-minikube.sh
@@ -20,3 +20,8 @@ echo "$(cat kubectl.sha256)  kubectl" | sha256sum --check
 chmod +x kubectl
 mv ./kubectl $HOME/.local/bin/kubectl
 rm -f kubectl.sha256
+
+curl -fsSL https://packages.buildkite.com/helm-linux/helm-debian/gpgkey | gpg --dearmor | sudo tee /usr/share/keyrings/helm.gpg > /dev/null
+echo "deb [signed-by=/usr/share/keyrings/helm.gpg] https://packages.buildkite.com/helm-linux/helm-debian/any/ any main" | sudo tee /etc/apt/sources.list.d/helm-stable-debian.list
+sudo apt update
+sudo apt install helm -y
