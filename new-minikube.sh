39a8ebc3d0a23c20ee6a3c7178b95f120731e4c3
Keiron <keiron.dang@gmail.com>
2025-11-25 14:34:58 +0700

    up Tue Nov 25 07:34:58 AM UTC 2025

--- new-minikube.sh
+++ new-minikube.sh
@@ -7,6 +7,15 @@ if [[ "$confirm" != "y" ]]; then
 	exit 1
 fi
 
+mkdir -p $HOME/.local/bin
+
 rm -f $HOME/.local/bin/minikube
 wget https://github.com/kubernetes/minikube/releases/latest/download/minikube-linux-amd64 -O $HOME/.local/bin/minikube
 chmod +x $HOME/.local/bin/minikube
+
+rm -f $HOME/.local/bin/kubectl
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl.sha256"
+echo "$(cat kubectl.sha256)  kubectl" | sha256sum --check
+chmod +x kubectl
+mv ./kubectl $HOME/.local/bin/kubectl
