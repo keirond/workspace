fcb1a1fa4e25c3b2777b88c2d3f680ffb0770814
Keiron <keiron.dang@gmail.com>
2025-11-25 15:45:08 +0700

    up Tue Nov 25 08:45:08 AM UTC 2025

--- new-minikube.sh
+++ new-minikube.sh
@@ -9,19 +9,31 @@ fi
 
 mkdir -p $HOME/.local/bin
 
-rm -f $HOME/.local/bin/minikube
-wget https://github.com/kubernetes/minikube/releases/latest/download/minikube-linux-amd64 -O $HOME/.local/bin/minikube
-chmod +x $HOME/.local/bin/minikube
+if minikube version &> /dev/null; then
+	echo "Minikube is already installed. Exiting."
+else
+	rm -f $HOME/.local/bin/minikube
+	wget https://github.com/kubernetes/minikube/releases/latest/download/minikube-linux-amd64 -O $HOME/.local/bin/minikube
+	chmod +x $HOME/.local/bin/minikube
+fi
 
-rm -f $HOME/.local/bin/kubectl
-curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
-curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl.sha256"
-echo "$(cat kubectl.sha256)  kubectl" | sha256sum --check
-chmod +x kubectl
-mv ./kubectl $HOME/.local/bin/kubectl
-rm -f kubectl.sha256
+if kubectl version --client &> /dev/null; then
+	echo "Kubectl is already installed. Exiting."
+else
+	rm -f $HOME/.local/bin/kubectl
+	curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+	curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl.sha256"
+	echo "$(cat kubectl.sha256)  kubectl" | sha256sum --check
+	chmod +x kubectl
+	mv ./kubectl $HOME/.local/bin/kubectl
+	rm -f kubectl.sha256
+fi
 
-curl -fsSL https://packages.buildkite.com/helm-linux/helm-debian/gpgkey | gpg --dearmor | sudo tee /usr/share/keyrings/helm.gpg > /dev/null
-echo "deb [signed-by=/usr/share/keyrings/helm.gpg] https://packages.buildkite.com/helm-linux/helm-debian/any/ any main" | sudo tee /etc/apt/sources.list.d/helm-stable-debian.list
-sudo apt update
-sudo apt install helm -y
+if helm version &> /dev/null; then
+	echo "Helm is already installed. Exiting."
+else
+	curl -fsSL https://packages.buildkite.com/helm-linux/helm-debian/gpgkey | gpg --dearmor | sudo tee /usr/share/keyrings/helm.gpg > /dev/null
+	echo "deb [signed-by=/usr/share/keyrings/helm.gpg] https://packages.buildkite.com/helm-linux/helm-debian/any/ any main" | sudo tee /etc/apt/sources.list.d/helm-stable-debian.list
+	sudo apt update
+	sudo apt install helm -y
+fi
