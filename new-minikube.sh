840cd2750f3359ee7532d1855bdc8e39973b7c6c
Crix Dang <crix.dang@gmail.com>
2025-11-25 14:15:03 +0700

    up Tue Nov 25 07:15:03 AM UTC 2025

--- new-minikube.sh
+++ new-minikube.sh
@@ -11,3 +11,7 @@ rm -rf $HOME/.local/minikube
 mkdir -p $HOME/.local/minikube
 wget https://github.com/kubernetes/minikube/releases/latest/download/minikube-linux-amd64 -O $HOME/.local/minikube-linux-amd64
 sudo install $HOME/.local/minikube-linux-amd64 "$HOME/.local/minikube" && rm $HOME/.local/minikube-linux-amd64
+
+echo "Minikube installed to $HOME/.local/minikube"
+echo "Add the following line to your ~/.bashrc or ~/.zshrc to use it:"
+echo 'export PATH=$HOME/.local/minikube:$PATH'
