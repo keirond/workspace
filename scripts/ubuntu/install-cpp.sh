d31484f6ed87e1a98e50cebbbbd5ce9552198eae
Crix Dang <crix.dang@gmail.com>
2025-12-19 01:50:17 +0700

    up Thu Dec 18 06:50:17 PM UTC 2025

---
+++ scripts/ubuntu/install-cpp.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -e
+
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+source "$SCRIPT_DIR/check-os.sh"
+
+sudo apt install build-essential gdb -y
+sudo apt install cmake ninja-build -y
+sudo apt install clang lldb llvm -y
+
+mkdir -p "$HOME/.local/bin/cppbin"
+cp -r "workspace/cppbin/." "$HOME/.local/bin/cppbin/"
+cp -f "workspace/.clang-format" "$HOME/.clang-format"
+
+CONFIG_NAME="C++"
+CONFIG_CONTENT='export PATH="$HOME/.local/bin/cppbin:$PATH"'
+source "$SCRIPT_DIR/add-auto-config.sh"
+
+echo "C++ workspace initialized and configured. Please restart your terminal or run 'source $SHELL_RC' to apply the changes."
