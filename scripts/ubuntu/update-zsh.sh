767bc93966574e03d66937ef13a563dcf1b0e9f6
Keiron <keiron.dang@gmail.com>
2025-12-19 01:46:49 +0700

    up Thu Dec 18 06:46:49 PM UTC 2025

--- scripts/ubuntu/update-zsh.sh
+++ scripts/ubuntu/update-zsh.sh
@@ -1,14 +1,15 @@
-#!/usr/bin/env zsh
+#!/bin/zsh
 set -e
 
-echo -n "This script is intended for Ubuntu systems only. Continue? (y/n): "
-read confirm
-if [[ "$confirm" != "y" ]]; then
-	echo "Aborted."
-	exit 1
-fi
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+source "$SCRIPT_DIR/check-os.sh"
 
 rm -f "$HOME/.zshrc"
+rm -rf ~/.bash* || true
+rm -rf ~/.profile || true
+rm -rf ~/.zcomp* || true
+rm -rf ~/.shell.pre-oh-my-zsh* || true
+rm -rf ~/.zshrc.pre-oh-my-zsh* || true
 
 if [ ! -d "$HOME/.oh-my-zsh/custom/plugins/zsh-autosuggestions" ]; then
 	git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
@@ -19,10 +20,5 @@ fi
 
 cp -f "workspace/keiron.zsh-theme" "$HOME/.oh-my-zsh/custom/themes/keiron.zsh-theme"
 cp -f "workspace/.zshrc" "$HOME/.zshrc"
-rm -rf ~/.bash* || true
-rm -rf ~/.profile || true
-rm -rf ~/.zcomp* || true
-rm -rf ~/.shell.pre-oh-my-zsh* || true
-rm -rf ~/.zshrc.pre-oh-my-zsh* || true
 
 echo "Zsh configuration updated. Please restart your terminal or run 'source ~/.zshrc' to apply the changes."
