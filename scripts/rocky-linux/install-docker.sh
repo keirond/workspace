bcebc0a85f5b660430ffb9c93ca6fefaf602ea55
Keiron <keiron.dang@gmail.com>
2025-12-19 12:45:57 +0700

    up Fri Dec 19 05:45:57 AM UTC 2025

--- scripts/rocky-linux/install-docker.sh
+++ scripts/rocky-linux/install-docker.sh
@@ -6,7 +6,7 @@ source "$SCRIPT_DIR/check-os.sh"
 
 sudo dnf install dnf-plugins-core -y
 sudo dnf config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
-sudo dnf install docker.ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+sudo dnf install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
 sudo systemctl enable --now docker
 sudo usermod -aG docker "$USER"
 echo "Docker installation completed. You may need to log out and log back in for group changes to take effect."
