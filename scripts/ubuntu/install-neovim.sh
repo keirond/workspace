b1bae6fed71ccc0a55fff5411e46e8ddcebe5fc5
Keiron <keiron.dang@gmail.com>
2025-12-26 01:43:53 +0700

    up Thu Dec 25 06:43:52 PM UTC 2025

---
+++ scripts/ubuntu/install-neovim.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -e
+
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux-x86_64.tar.gz
+sudo rm -rf ~/.local/nvim/nvim-linux-x86_64
+mkdir -p ~/.local/nvim
+sudo tar -C ~/.local/nvim -xzf nvim-linux-x86_64.tar.gz
+sudo rm -rf nvim-linux-x86_64.tar.gz
+
+mkdir -p "$HOME/.config/nvim"
+cp -f "workspace/init.lua" "$HOME/.config/nvim/init.lua"
+
+CONFIG_NAME="neovim"
+CONFIG_CONTENT='export PATH="$HOME/.local/nvim/nvim-linux-x86_64/bin:$PATH"'
+source "$SCRIPT_DIR/add-auto-config.sh"
+
+echo "Neovim installation and configuration completed. You can start Neovim by running the "nvim" command."
